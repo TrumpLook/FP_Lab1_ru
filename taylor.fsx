d925a9192ce772ca25222a7969be5b26a1d3ae25
TrumpLook <82106445+TrumpLook@users.noreply.github.com>
2024-09-05 21:50:36 +0300

    Update taylor.fsx

--- taylor.fsx
+++ taylor.fsx
@@ -1,36 +1,38 @@
 open System
 
 // Встроенная функция F#
-let builtinExp2x x = Math.Exp(2.0 * x)
+let f x = Math.Exp(2.0 * x)
+
+// Интервал [0.0, 1.0] с шагом 0.1
+let a = 0.0
+let b = 1.0
+let n = 10
+let interval = [a .. (b - a) / float n .. b]
 
 // Наивный способ вычисления ряда Тейлора
-let naiveTaylorExp2x x eps =
-    let rec loop n term sum =
-        if abs term < eps then (sum, n)
+let nt x eps =
+    let rec l k t sum =
+        if abs t < eps then (sum, k)
         else
-            let term = term * (2.0 * x) / float n
-            loop (n + 1) term (sum + term)
-    loop 1 (2.0 * x) 1.0
+            let t = t * (2.0 * x) / float k
+            l (k + 1) t (sum + t)
+    l 1 (2.0 * x) 1.0
 
 // Умный способ вычисления ряда Тейлора
-let smartTaylorExp2x x eps =
-    let mutable n = 0
-    let mutable term = 2.0 * x
+let st x eps =
+    let mutable k = 0
+    let mutable t = 2.0 * x
     let mutable sum = 1.0
-    while abs term >= eps do
-        n <- n + 1
-        term <- term * 2.0 * x / float n
-        sum <- sum + term
-    sum, n
-
-// Интервал [0.1, 0.6] с шагом 0.1
-let interval = [0.1 .. 0.1 .. 0.6]
-let eps = 1e-10
+    while abs t >= eps do
+        k <- k + 1
+        t <- t * 2.0 * x / float k
+        sum <- sum + t
+    sum, k
 
 // Вывод результатов
 printfn "x\tBuiltin\t\tNaive\t\tNaive Terms\tSmart\t\tSmart Terms"
 for x in interval do
-    let builtin = builtinExp2x x
-    let (naive, naiveTerms) = naiveTaylorExp2x x eps
-    let (smart, smartTerms) = smartTaylorExp2x x eps
-    printfn "%.1f\t%.10f\t%.10f\t%d\t\t%.10f\t%d" x builtin naive naiveTerms smart smartTerms
+    let b = f x
+    let (n, ntk) = nt x eps
+    let (s, stk) = st x eps
+    printfn "%.1f\t%.10f\t%.10f\t%d\t\t%.10f\t%d" x b n ntk s stk
